4e7c7925baf5b94399c7d0196427acc6d4721def
Noohaljabri1998 <90241333+Noohaljabri1998@users.noreply.github.com>
2021-09-13 12:44:08 +0400

    Create head.php

---
+++ head.php
@@ -0,0 +1,9 @@
+<title>aljabri</title>
+<link rel="stylesheet" href="style.css">
+<link rel="icon" href="logo.svg" type="image/x-icon">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    </head>
+    <body>
+        <div></div>
+        <div></div>
