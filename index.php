010d28909fd53638d4671321d1827c506c8b1fd6
Noohaljabri1998 <90241333+Noohaljabri1998@users.noreply.github.com>
2021-09-13 12:31:15 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/main.css">
+        <?php require_once "head.php";?>
+        <header>
+            <?php require_once 'nav.php'?>
+                <span class="search">
+                    <a href="login"><svg viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M15 16s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002zM5.022 15h9.956a.274.274 0 00.014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C13.516 12.68 12.289 12 10 12c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 00.022.004zm9.974.056v-.002zM10 9a2 2 0 100-4 2 2 0 000 4zm3-2a3 3 0 11-6 0 3 3 0 016 0z" clip-rule="evenodd"></path></svg></a>
+                </span>
+            </nav>
+            <div class="clock">
+                <div class="h"></div>
+                <div class="m"></div>
+                <div class="s"></div>
+                <div class="cir"></div>
+            </div>
+        </header>
+        <main>
+            <section class="s1">
+                <h1>خدماتنا</h1>
+                <div>
+                    <div><img src="img/1.svg"><h2>تصميم</h2></div>
+                    <div><img src="img/1.svg"><h2>تصميم علب</h2></div>
+                    <div><img src="img/1.svg"><h2>تصميم مواقع</h2></div>
+                </div>
+            </section>
+        </main>
+        <script src="main.js"></script>
+    </body>
+</html>
