b5a918437ccf1cd2d5ecf949ce73146e1906ac5d
Noohaljabri1998 <90241333+Noohaljabri1998@users.noreply.github.com>
2021-09-18 06:32:51 +0400

    del

--- head.php
+++
@@ -1,9 +0,0 @@
-<title>aljabri</title>
-<link rel="stylesheet" href="style.css">
-<link rel="icon" href="logo.svg" type="image/x-icon">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    </head>
-    <body>
-        <div></div>
-        <div></div>
